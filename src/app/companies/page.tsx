bc14092089e88ffb503d9d9a811f245433a77532
Mariia <shymanskam524@gmail.com>
2025-05-27 13:20:44 +0200

    add file companies and icons

---
+++ src/app/companies/page.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import Header from '../componenets/header';
+import Toolbar from '../componenets/toolbar';
+import SearchInput from '../componenets/search-input';
+import AddCompanyButton from '../componenets/add-company-button';
+import CompanyTable from '../componenets/company-table';
+import CompanyRow from '../componenets/company-row';
+import { Status } from '../componenets/status-label';
+
+export interface PageProps {}
+
+export default function Page({}: PageProps) {
+  return (
+    <>
+      <Header>Companies</Header>
+      <main>
+        <Toolbar action={<AddCompanyButton />}>
+          <SearchInput />
+        </Toolbar>
+        <CompanyTable>
+          <CompanyRow
+            id={1}
+            category="Products"
+            company="Costco"
+            status={Status.Pending}
+            promotion={true}
+            country="USA"
+            joinedDate="02.19.2023"
+          />
+        </CompanyTable>
+      </main>
+    </>
+  );
+}
